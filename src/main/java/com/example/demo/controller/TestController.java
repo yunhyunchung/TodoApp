19ed5733f76d97a2c79ce9889cb4ad61e3ef5a0c
= <=>
2023-03-20 16:04:39 +0900

    3/20 수업 완료

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import lombok.Data;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test")
@@ -11,10 +15,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/testGetMapping")
     public  String testController() {
-        return "Hello World! testGetMapping";
+        return "Hello World! 정윤현";
     }
     @GetMapping("/{id}")
     public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
-        return "Hello World! ID " + id;
+        return "Hello World! 정윤현 " + id;
+    }
+    @GetMapping("/testRequestParam")
+    public String testControllerRequestParam(@RequestParam(required = false) int id) {
+        return "Hello World! 정윤현" + id;
+    }
+    @GetMapping("/testRequestBody")
+    public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+        return "Hello World! 정윤현.id: " + testRequestBodyDTO.getId()
+                + ", message: " + testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testResponseBody")
+    public ResponseDTO<String> testControllerResponseBody() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! I'm ResponseDTO 정윤현");
+        list.add("yhc1");
+        list.add("yhc2");
+        list.add("yhc3");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return response;
+    }
+
+    @GetMapping("/testResponseEntity")
+    public ResponseEntity<?> testControllerResponseEntity() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! I'm ResponseEntity. And you got 400!");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        // http response를 서버 상태를 200으로 설정
+        return ResponseEntity.ok().body(response);
+        // http response를 서버 상태를 400으로 설정
+        //return ResponseEntity.badRequest().body(response);
     }
 }
---
+++ src/main/java/com/example/demo/dto/TestRequestBodyDTO.java
@@ -0,0 +1,8 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+@Data
+public class TestRequestBodyDTO {
+    private int id;
+    private String message;
+}
